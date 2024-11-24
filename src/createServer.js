e5f601d58b0a3ff573414e617bba098fb576377e
4xmplme <andrii_kondratenko@icloud.com>
2024-11-25 01:33:57 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,71 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const [path, queryString] = req.url.split('?');
+    const errors = [];
+
+    if (!path || path === '/') {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    const textToConvert = path.slice(1);
+
+    if (!textToConvert) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    const searchParams = new URLSearchParams(queryString);
+    const toCase = searchParams.get('toCase');
+
+    if (!toCase) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    if (toCase && !validCases.includes(toCase)) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+    }
+
+    const { originalCase, convertedText } = convertToCase(
+      textToConvert,
+      toCase,
+    );
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        originalCase,
+        targetCase: toCase,
+        originalText: textToConvert,
+        convertedText,
+      }),
+    );
+  });
+}
+
+module.exports = { createServer };
